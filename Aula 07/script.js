5d4987c269ddb11fb4b2fd3f891c6cb4eb6c65b4
antoninhobrasil0906-coder <antoninhobrasil0906@gmail.com>
2026-03-19 15:53:29 -0300

    feat:Aula 7 adicionado

---
+++ Aula 07/script.js
@@ -0,0 +1,11 @@
+const user = {
+    nome: "Ana",
+    sobrenome: "Conti",
+    idade: 24,
+    sayHello: function(){alert("Hello")}
+}
+
+alert(user.nome)
+console.log(user.nome)
+user.sayHello()
+
